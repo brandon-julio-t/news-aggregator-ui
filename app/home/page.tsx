936f632583abec5f4634f1dfdf0406322ea70128
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-06-09 19:32:47 +0700

    feat: better home page layout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,25 @@
 'use client';
 
+import Card from '@/components/common/Card';
 import Articles from './components/Articles';
 import Filter from './components/Filters/Filter';
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <Filter />
+    <div className="flex flex-col lg:flex-row gap-4">
+      <section className="flex-1">
+        <Card>
+          <Filter />
+        </Card>
+      </section>
 
-      <h1 className="my-2 text-2xl font-bold">Explore Articles</h1>
+      <section className="flex-[3]">
+        <Card>
+          <h1 className="my-2 text-2xl font-bold">Explore Articles</h1>
 
-      <Articles baseApiPath="/api/articles" />
+          <Articles baseApiPath="/api/articles" />
+        </Card>
+      </section>
     </div>
   );
 };
