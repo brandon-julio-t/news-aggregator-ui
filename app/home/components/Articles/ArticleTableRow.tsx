0b04312268ff07d5714c3777794cb85173f59496
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-06-10 13:22:34 +0700

    feat: like/dislike button in table

--- app/home/components/Articles/ArticleTableRow.tsx
+++ app/home/components/Articles/ArticleTableRow.tsx
@@ -2,6 +2,7 @@ import React, { ComponentProps, ComponentType } from 'react';
 import Article from '@/lib/contracts/Article';
 import PaginationResponse from '@/lib/contracts/PaginationResponse';
 import Link from 'next/link';
+import LikeOrDislikeUserArticlePreferenceButton from '@/components/user-article-preference/LikeOrDislikeUserArticlePreferenceButton';
 
 interface IArticleTableRow {
   article: Article;
@@ -19,9 +20,17 @@ const ArticleTableRow: ComponentType<ComponentProps<'tr'> & IArticleTableRow> = 
     <tr key={article.id} {...rest}>
       <th>{(pagination.current_page - 1) * pagination.per_page + (index + 1)}</th>
       <td>{article.title}</td>
-      <td>{article.author}</td>
       <td>
-        <span className="badge badge-primary badge-outline whitespace-nowrap">{article.category}</span>
+        <div className="flex flex-nowrap gap-2 items-center">
+          {article.author}
+          <LikeOrDislikeUserArticlePreferenceButton noLabel type="author" value={article.author} />
+        </div>
+      </td>
+      <td>
+        <div className="flex flex-nowrap gap-2 items-center">
+          <span className="badge badge-primary badge-outline whitespace-nowrap">{article.category}</span>
+          <LikeOrDislikeUserArticlePreferenceButton noLabel type="category" value={article.category} />
+        </div>
       </td>
       <td>{new Date(article.created_at).toString()}</td>
       <td>
